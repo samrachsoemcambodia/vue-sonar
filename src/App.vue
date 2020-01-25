664d0deef51c211b248db62ffda1f7b6988b1127
soem samrach <soemsamrach15@kit.edu.kh>
2020-01-25 09:37:30 +0700

    testing again

--- src/App.vue
+++ src/App.vue
@@ -25,4 +25,18 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
 </style>
